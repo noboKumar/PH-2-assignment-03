ff46cad33b682bcf80196140b29033183e6005c6
noboKumar <nobokumar227@gmail.com>
2025-12-31 02:48:14 +0600

    add: vehicles table create

--- query.sql
+++ query.sql
@@ -1,9 +1,23 @@
-CREATE Table IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
-    role VARCHAR(10) NOT NULL DEFAULT 'customer' CHECK (role IN ('admin', 'customer')),
+    role NOT NULL DEFAULT 'customer' CHECK (role IN ('admin', 'customer')),
     user_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     CHECK (email = lower(email)),
     password text NOT NULL,
     phone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vehicles (
+    vehicle_id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(50) NOT NULL,
+    registration_number VARCHAR(50) UNIQUE NOT NULL,
+    daily_rent_price INT NOT NULL,
+    availability_status VARCHAR(20) NOT NULL DEFAULT 'available' CHECK (
+        availability_status IN (
+            'available',
+            'rented',
+            'maintenance'
+        )
+    )
 );
