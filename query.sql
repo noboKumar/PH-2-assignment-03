d3bc1e8fa6bf71d7b3c42c604d9064b7e6e772e5
noboKumar <nobokumar227@gmail.com>
2025-12-31 04:32:55 +0600

    add: get booking information with customer name and vehicle name

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+-- users table
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     role NOT NULL DEFAULT 'customer' CHECK (role IN ('admin', 'customer')),
@@ -8,9 +9,14 @@ CREATE TABLE IF NOT EXISTS users (
     phone VARCHAR(20) NOT NULL
 );
 
+-- vehicle table
 CREATE TABLE IF NOT EXISTS vehicles (
     vehicle_id SERIAL PRIMARY KEY,
     vehicle_name VARCHAR(50) NOT NULL,
+    vehicle_type VARCHAR(50) NOT NULL CHECK (
+        vehicle_type IN ('car', 'bike', 'truck')
+    ),
+    model INT NOT NULL,
     registration_number VARCHAR(50) UNIQUE NOT NULL,
     daily_rent_price INT NOT NULL,
     availability_status VARCHAR(20) NOT NULL DEFAULT 'available' CHECK (
@@ -22,14 +28,15 @@ CREATE TABLE IF NOT EXISTS vehicles (
     )
 );
 
+-- bookings table
 CREATE TABLE IF NOT EXISTS bookings (
     booking_id SERIAL PRIMARY KEY,
     booked_user_id INT REFERENCES users (user_id) ON DELETE CASCADE,
     booked_vehicle_id INT REFERENCES vehicles (vehicle_id) ON DELETE CASCADE,
     start_date DATE,
     end_date DATE,
-    status VARCHAR(20) NOT NULL CHECK (
-        status IN (
+    booking_status VARCHAR(20) NOT NULL CHECK (
+        booking_status IN (
             'pending',
             'confirmed',
             'completed',
@@ -38,3 +45,17 @@ CREATE TABLE IF NOT EXISTS bookings (
     ),
     total_price NUMERIC(10, 2) NOT NULL
 );
+
+-- booking information with customer name and vehicle name
+select
+    booking_id,
+    user_name as customer_name,
+    vehicle_name,
+    start_date,
+    end_date,
+    booking_status as status
+from
+    bookings as b
+    join users as u on b.booked_user_id = u.user_id
+    join vehicles as v on b.booked_vehicle_id = v.vehicle_id
+
